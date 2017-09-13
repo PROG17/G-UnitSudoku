b04044d93f3908d479250bc253f05ef73d2979a3
Gustav Westberg <gustavwes@gmail.com>
2017-09-13 15:37:28 +0200

    Added boardprintout

--- Sudoku/Sudoku/Board.cs
+++ Sudoku/Sudoku/Board.cs
@@ -11,7 +11,7 @@ namespace Sudoku
         private const int height = 9;
         private const int width = 9;
 
-        Square[,] squares = new Square[height,width];
+        Square[,] squares = new Square[height, width];
 
         public Board(string inputNumbers)
         {
@@ -20,12 +20,35 @@ namespace Sudoku
                 for (int column = 0; column < width; column++)
                 {
                     int index = column + row * 9;
-                    int number = int.Parse( inputNumbers[index].ToString() );
+                    int number = int.Parse(inputNumbers[index].ToString());
 
                     Square square = new Square(number);
                     squares[row, column] = square;
                 }
             }
+        }
+
+        public void PrintBoard()
+        {
+            for (int row = 0; row < height; row++)
+            {
+                Console.WriteLine("-------------------------");
+                Console.Write("| ");
+                for (int column = 0; column < width; column++)
+                {
+                    int squareNumber = squares[row, column].SquareValue;
+                    Console.Write(squareNumber+ " ");
+
+                    if (column % 3 == 2)
+                    {
+                        Console.Write("| ");
+                    }
+                }
+
+                
+                Console.WriteLine();
+            }
+            Console.WriteLine("-------------------------");
         }
     }
 }
--- Sudoku/Sudoku/Program.cs
+++ Sudoku/Sudoku/Program.cs
@@ -11,6 +11,8 @@ namespace Sudoku
         static void Main(string[] args)
         {
             Board board = new Board("619030040270061008000047621486302079000014580031009060005720806320106057160400030");
+            board.PrintBoard();
+            
         }
     }
 }
