83ba46d906be4f790e419fc5902d806e02c32b70
Victor Hermansson <hermansson_79@hotmail.com>
2017-09-14 15:03:16 +0200

    Kommenterat kod

--- Sudoku/Sudoku/Sudoku.cs
+++ Sudoku/Sudoku/Sudoku.cs
@@ -13,65 +13,95 @@ namespace Sudoku
 
         public Sudoku(string numbers)
         {
-            board = new Board(numbers);
+            board = new Board(numbers);     // Skapa upp sudokubräde.
 
         }
-        public void Solved()
+        
+        public void Solve()
         {
-            bool somethingChanged = true;
+            bool somethingChanged = true;   // Variabel som håller koll på om solvern fortafarande kan göra någongting. 
             while (somethingChanged)
             {
                 somethingChanged = false;
 
-                for (int row = 0; row < 9; row++)
-                {
-                    List<int> numbers = board.GetNumbersInRow(row);
-                    for (int column = 0; column < 9; column++)
-                    {
-                        //om något ändrats kör loopen igen
-                        if (board.SendNumbersToSquare(row, column, numbers))
-                        {
-                            somethingChanged = true;
-                        }
-                    }
-                }
-                
+                // Testa logik per rad.
+                if (CheckRows()) somethingChanged = true;
+                // 
+                if (CheckColumns()) somethingChanged = true;
+
+                if (CheckBoxes()) somethingChanged = true;
+
+            }
+
+            board.PrintBoard();
+        }
+
+        public bool CheckRows()
+        {
+            bool somethingChanged = false;
+
+            // Lopar igen alla rader.
+            for (int row = 0; row < 9; row++)
+            {
+                List<int> numbers = board.GetNumbersInRow(row);
                 for (int column = 0; column < 9; column++)
                 {
-                    List<int> numbers = board.GetNumbersInColumn(column);
-                    for (int row = 0; row < 9; row++)
+                    // Om raden har fått mera information så har något ändrats.
+                    if (board.SendNumbersToSquare(row, column, numbers))
                     {
-                        //om något ändrats kör loopen igen
-                        if (board.SendNumbersToSquare(row, column, numbers))
-                        {
-                            somethingChanged = true;
-                        }
+                        somethingChanged = true;
                     }
                 }
-                
-                
-                for (int boxRow = 0; boxRow < 3; boxRow++)
+            }
+
+            return somethingChanged;
+        }
+
+        public bool CheckColumns()
+        {
+            bool somethingChanged = false;
+            for (int column = 0; column < 9; column++)
+            {
+                List<int> numbers = board.GetNumbersInColumn(column);
+                for (int row = 0; row < 9; row++)
                 {
-                    for (int boxColumn = 0; boxColumn < 3; boxColumn++)
+                    //om något ändrats kör loopen igen
+                    if (board.SendNumbersToSquare(row, column, numbers))
                     {
-                        List<int> numbers = board.GetNumberInBox(boxRow, boxColumn);
+                        somethingChanged = true;
+                    }
+                }
+            }
 
-                        for (int row = boxRow * 3; row < 3 * (boxRow + 1); row++)
+            return somethingChanged;
+        }
+        
+        public bool CheckBoxes()
+        {
+            bool somethingChanged = false;
+
+            for (int boxRow = 0; boxRow < 3; boxRow++)
+            {
+                for (int boxColumn = 0; boxColumn < 3; boxColumn++)
+                {
+                    List<int> numbers = board.GetNumberInBox(boxRow, boxColumn);
+
+                    for (int row = boxRow * 3; row < 3 * (boxRow + 1); row++)
+                    {
+                        for (int column = boxColumn * 3; column < 3 * (boxColumn + 1); column++)
                         {
-                            for (int column = boxColumn * 3; column < 3 * (boxColumn + 1); column++)
-                            {
 
-                                if (board.SendNumbersToSquare(row, column, numbers))
-                                {
-                                    somethingChanged = true;
-                                }
+                            if (board.SendNumbersToSquare(row, column, numbers))
+                            {
+                                somethingChanged = true;
                             }
                         }
                     }
                 }
             }
 
-            board.PrintBoard();
+            return somethingChanged;
         }
+    
     }   
 }   
